242e6c5f5c1342e97ef8c55ea53855960978b5d3
Nitishkumar Singh <nitishkumarsingh71@gmail.com>
2023-05-16 20:12:34 +0530

    add deployment code

--- cto_test/main.go
+++ cto_test/main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
-	ctoai "github.com/cto-ai/sdk-go/v2"
 	"net/http"
+
+	ctoai "github.com/cto-ai/sdk-go/v2"
 )
 
 func main() {
 	client := ctoai.NewClient()
 	client.Ux.Print("starting the server")
+
+	event := map[string]interface{}{
+		"event_name":   "deployment",
+		"event_action": "succeeded",
+		"branch":       "main",
+		"repo":         "cto_test",
+		"environment":  "production",
+	}
+
+	err := client.Sdk.Track([]string{}, "", event)
+	if err != nil {
+		panic(err)
+	}
 
 	http.HandleFunc("/", HelloServer)
 	http.ListenAndServe(":8080", nil)
